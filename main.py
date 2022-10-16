84958fb6468f146759e584fc6d79891494a39b66
Colin Acerbi <colinacerbi@gmail.com>
2022-10-16 19:22:57 -0400

    add preferred moves and improve output formatting

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import datetime
-import json
-from multiprocessing.connection import wait
-from xml.dom.minidom import Attr
+from time import sleep
+from datetime import datetime
+
+from json import dump
+
+from random import choice
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
-from time import sleep
-from random import choice
+
+
 
 ARRS_URL = "https://arrs.host"   
 
@@ -20,7 +22,7 @@ WARDEN_USER = "warden"
 WARDEN_PASSWORD = "O5SXIMZRO5QXIZLS"
 DECRYPT_STRING = "decrypt /home/warden/private/d900a70d64.inf"
 # start is used to start the maze loop, not a real output
-EXPECTED_OUT = {"start", "true", "false", "you died", "blocked 30s"}
+EXPECTED_OUT = {"start", "true", "false", "you died", "blocked 30s", "teleported"}
 
 move_opposites = {
         "up": "down",
@@ -55,6 +57,33 @@ def move(move_text):
     print(response)
     return response
 
+
+def calc_available_moves(x,y):
+    # up, right, down, left
+    return [(x, y+1), (x+1, y), (x, y-1), (x-1, y)]
+
+def get_preferred_move(x, y, opens, walls, blocks):
+
+    move_strings = ["up", "right", "down", "left"]
+
+    available_moves = calc_available_moves(x, y)
+    preferred_moves = []
+
+    # check for non-visited nodes first
+    for i, move in enumerate(available_moves):
+        if move not in opens and move not in walls and move not in blocks:
+            preferred_moves.append(move_strings[i]) 
+        
+    if preferred_moves:
+        return choice(preferred_moves)
+
+    # if there are no non-visited nodes, start backtracking
+    for i, move in enumerate(available_moves):
+        if move not in walls:
+            preferred_moves.append(move_strings[i])
+
+    return choice(preferred_moves)
+
 def maze_run():
 
     move_coords = {
@@ -65,10 +94,17 @@ def maze_run():
         }
 
     history = []
-    opens = set()
-    walls = set()
-    unexpected = "Nothing unexpected"
 
+    opens = set()
+    opens.add(0,0)
+
+    walls = set()
+    blocks = set()
+    kills = set()
+    teleports = set()
+
+    unexpected = False
+    teleported = False
     response = "start"
     # previous move is the text of the move, i.e "left"
     previous_move = "up"
@@ -89,8 +125,9 @@ def maze_run():
             x += move_coords[previous_move][0]
             y += move_coords[previous_move][1]
             opens.add((x,y))
+
             # move anywhere that is not the previous move (i.e. don't backtrack)
-            next_move = choice([m for m in move_strings if m not in move_opposites[previous_move]])
+            next_move = get_preferred_move(x, y, opens, walls, blocks)
             response = move(next_move)
             previous_move = next_move
 
@@ -100,43 +137,77 @@ def maze_run():
             walls.add((wall_x, wall_y))
 
             # move anywhere that is not the move that just failed (i.e. don't move into a known wall)
-            next_move = choice([m for m in move_strings if m not in previous_move])
+            next_move = next_move = get_preferred_move(x, y, opens, walls, blocks)
             response = move(next_move)
             previous_move = next_move
 
         elif response == "blocked 30s":
             print(f"Waiting at f({x}, {y})")
             sleep(31)
+            
+            x += move_coords[previous_move][0]
+            y += move_coords[previous_move][1]
+            blocks.add((x, y))
+
             # move anywhere that is not the previous move (i.e. don't backtrack)
-            next_move = choice([m for m in move_strings if m not in move_opposites[previous_move]])
+            next_move = get_preferred_move(x, y, opens, walls, blocks)
             response = move(next_move)
             previous_move = next_move
 
         elif response == "you died":
+            x += move_coords[previous_move][0]
+            y += move_coords[previous_move][1]
+
             print(f"Dead after {len(history)} nodes")
             print('=' * 15)
+            running = False
+        
+        elif response == "teleported":
+            teleported = True
+
+            x += move_coords[previous_move][0]
+            y += move_coords[previous_move][1]
+            teleports.add((x,y))
+
             running = False
     
         elif response == "start":
             response = move(choice([m for m in move_strings]))
 
-        
+    if unexpected:
+        result_prefix = "Unexpected"
+    elif teleported:
+        result_prefix = "Teleported"
+    else:
+        result_prefix = "Died"
 
+    with open(result_prefix + '_MazeRun' + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.txt', 'w') as f:
+        f.write(f"{result_prefix} at ({x}, {y}): {response}\n")
     
-    with open('MazeRun' + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + '.txt', 'w') as f:
-        if unexpected != "Nothing unexpected":
-            f.write(f"UNEXPECTED OUTPUT FOUND at ({x}, {y}): {response}")
-
-        f.write(f"Result: {response}")
+        f.write(f"Result: {response}\n")
 
         f.write("Move history:\n")
-        json.dump(history, f)
+        dump(history, f)
+        f.write("\n\n")
 
         f.write("Walls: \n")
-        json.dump(list(walls), f)
+        dump(list(walls), f)
+        f.write("\n\n")
 
         f.write("Open Squares: \n")
-        json.dump(list(opens), f)
+        dump(list(opens), f)
+        f.write("\n\n") 
+
+        f.write("Block (Delay) Squares: \n")
+        dump(list(opens), f)
+        f.write("\n\n")
+
+        f.write("Kill Squares (if applicable): \n")
+        dump(list(kills), f)
+        f.write("\n\n") 
+
+        f.write("Teleport Squares (if applicable): \n")
+        dump(list(teleports), f)
 
 
 # Login w/ username
@@ -154,4 +225,6 @@ input_field = wait_until_available(INPUT_XPATH)
 input_field.send_keys(DECRYPT_STRING)
 input_field.send_keys(Keys.RETURN)
 
-maze_run()
+searching = True
+while searching:
+    maze_run()
